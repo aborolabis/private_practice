079c49f5efacfdeb6558e5d5966db969d50e5fe9
izabela kopczynska <izabela.anna.k@gmail.com>
2018-06-25 15:03:05 +0200

    Initial commit

---
+++ src/main/java/pl/sda/aborolabis/projects/private_practice/PrivatePracticeApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.aborolabis.projects.private_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivatePracticeApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/sda/aborolabis/projects/private_practice/PrivatePracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.aborolabis.projects.private_practice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrivatePracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
